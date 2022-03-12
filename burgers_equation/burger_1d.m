63be00c971c587c00e4963efd56b8aab4c879a77
microwerx <jmetzgar@outlook.com>
2022-03-12 11:37:34 -0800

    Burgers equation

--- burgers_equation/burger_1d.m
+++ burgers_equation/burger_1d.m
@@ -5,14 +5,14 @@
 
 clear;clc;close all;
 
-Length = 10;
+Length = 4*pi;
 NSteps = 101;
 dx = Length/NSteps;
 
 %% Create our initial conditions.
 
 x = linspace(0, Length, NSteps);
-u = log(2 .+ sin(x));
+u = 0.5 + 0.5*log(3 .+ sin(x));
 plot(x, u);
 axis([0, Length, -2, 2]);
 umax = max(u);
@@ -34,7 +34,7 @@ dydx = @(y1, y2, dx) (y2 - y1) / dx;
 
 %% Set up the loop to run a simulation over time.
 ntime = 20
-dt = .1
+dt = 0.1
 
 t = 0;
 i = 0;
@@ -56,8 +56,19 @@ while t < ntime+dt
     u_1 = u(k);
     u_2 = u(k2);
     
-    du_dx(k) = (u(k) - u(k0)) / dx;
+    %%Backward differences
+##    du_dx(k) = (u_1 - u_0) / dx;
+    %%Central differences
+    du_dx(k) = (u_2 - u_0) / (2*dx);
+    %%Upwind differences
+##    if u_1 >= 0
+##      du_dx(k) = (u_1 - u_0) / dx;
+##    else
+##      du_dx(k) = (u_2 - u_1) / dx;
+##    endif
     %%du_dx(k) = max(du_dx(k), 1000);
+    %% clamping
+##    du_dx(k) = clamp(du_dx(k), -2, 2);
 ##    du_dx(k) = clamp(du_dx(k), -umax, umax);
 ##    if isnan(du_dx(k)) || isinf(du_dx(k))
 ##      du_dx(k) = 0;
@@ -68,7 +79,7 @@ while t < ntime+dt
 ##      du_dt(k) = 0;
 ##    endif    
 ##    unext(k) = clamp(u(k) + dt * du_dt(k), -umax, umax);
-    unext(k) = u(k) + dt * du_dt(k);
+    unext(k) = u(k) + dt * du_dt(k) - 0.001 * u(k);
   endfor
   u = unext;
   
